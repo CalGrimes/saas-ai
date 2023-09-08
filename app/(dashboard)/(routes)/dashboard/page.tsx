a6afb1858a2ce620ca536eaa5ebfab25a2939fbb
CalGrimes <cal.grimes@outlook.com>
2023-09-08 17:08:33 +0100

    Added Route Groups
    - Removed main page
    - Landing Page
    - Dashboard Page

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+     <Button variant="default">Click Me</Button>
+    )
+}
+
+export default DashboardPage;
