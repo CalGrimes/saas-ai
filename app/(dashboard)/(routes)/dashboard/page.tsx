9c6d47f58cd7e48c236308276ffa81fece1808be
CalGrimes <cal.grimes@outlook.com>
2023-09-09 12:46:11 +0100

    Sign-Up/Sign-in
    Sign-in and Sign-up implemented using Clerk

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
     return (
-     <Button variant="default">Click Me</Button>
+     <div>
+        <p>Dashboard Page (Protected)</p>
+        <UserButton />
+    </div>
     )
 }
 
